86e6aee3cbd895c3d8490898c92e0b56580837b4
luthfiansyh <muhammadluthfiansyah15@gmail.com>
2020-03-09 21:07:28 +0700

    tugas praktikum 2

---
+++ TugasAnalgo2/1-Nilaimax.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {2,4,8,16,32};
+    int panjangArr = sizeof(arr)/sizeof(arr[0]);
+    int maks = arr[0];
+    int i = 2;
+    
+    while(i<panjangArr){
+        if(arr[i] > maks){
+            maks = arr[i];
+        }
+        i++;
+    }
+    
+    cout<<"\n Nilai maks dari array: "<<maks;
+}
+
